ead1cb6a38cc1baa63ce6e286746d260d3f7143d
oguzkuseli <oguz.kuseli@hotmail.com>
2023-12-24 11:56:18 +0300

    Initial Commit

---
+++ Odev3/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Odev3
+//
+//  Created by Oğuz Kuşeli on 23.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "GymBros"
+        
+        let appearance = UINavigationBarAppearance()
+        
+        appearance.backgroundColor = UIColor(named: "anaRenk")
+        appearance.titleTextAttributes = [.foregroundColor : UIColor(named: "yaziRenk1")!, .font : UIFont(name: "FjallaOne-Regular", size: 22)!]
+        
+        navigationController?.navigationBar.barStyle = .black
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        
+        navigationController?.navigationBar.compactAppearance = appearance
+        
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+    }
+
+
+}
+
